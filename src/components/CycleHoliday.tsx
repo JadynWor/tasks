0272bb4690985f7a4f282f65c7320e4e907ac60b
Jadyn Worthington <jadynwor@udel.edu>
2023-03-28 17:35:28 -0400

    solved

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,37 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holiday = "🪔" | "🎨" | "🎃" | "🎄" | "🦃";
+    const Time_Transitions: Record<Holiday, Holiday> = {
+        "🎨": "🪔",
+        "🪔": "🎃",
+        "🎃": "🦃",
+        "🦃": "🎄",
+        "🎄": "🎨"
+    };
+    const Alphabet_Transitions: Record<Holiday, Holiday> = {
+        "🎄": "🪔",
+        "🪔": "🎃",
+        "🎃": "🎨",
+        "🎨": "🦃",
+        "🦃": "🎄"
+    };
+    function changeTime(): void {
+        const newHoli2 = Time_Transitions[holi];
+        setHoliday(newHoli2);
+    }
+    const [holi, setHoliday] = useState<Holiday>("🎄");
+    function changeAlphabet(): void {
+        const newHoli = Alphabet_Transitions[holi];
+        setHoliday(newHoli);
+    }
+    return (
+        <div>
+            <div>
+                <p> Holiday: {holi}</p>
+                <Button onClick={changeTime}>Advance by Year</Button>
+                <Button onClick={changeAlphabet}>Advance by Alphabet</Button>
+            </div>
+        </div>
+    );
 }
