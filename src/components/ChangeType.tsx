a743cf85b4a5f149dc15464a27b9b9bb141752f6
Jadyn Worthington <jadynwor@udel.edu>
2023-03-28 22:13:49 -0400

    done

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,29 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questype, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    const Question_Transitions: Record<QuestionType, QuestionType> = {
+        multiple_choice_question: "short_answer_question",
+        short_answer_question: "multiple_choice_question"
+    };
+    function changeQuestionType(): void {
+        const newQuestion = Question_Transitions[questype];
+        setQuestionType(newQuestion);
+    }
+    return (
+        <div>
+            <div>
+                <Button onClick={changeQuestionType}>Change Type</Button>
+            </div>
+            <div>
+                {questype === "short_answer_question" ? (
+                    <span>Short Answer</span>
+                ) : (
+                    <span>Multiple Choice</span>
+                )}
+            </div>
+        </div>
+    );
 }
