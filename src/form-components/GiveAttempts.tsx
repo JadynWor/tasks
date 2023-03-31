57a9c3bdd2f4f8409db3a7c08b7f96b17c6127c5
Jadyn Worthington <jadynwor@udel.edu>
2023-03-31 19:51:27 -0400

    task 10 uploaded

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 export function GiveAttempts(): JSX.Element {
+    const [attempt, setAttempt] = useState<number>(3);
+    const [request, setRequest] = useState<number>(1);
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <h3>Give attempts</h3>
+            <Form.Group controlId="formGiveAttempts">
+                <Form.Control
+                    value={request}
+                    type="number"
+                    onChange={(event: ChangeEvent) =>
+                        setRequest(parseInt(event.target.value) || 0)
+                    }
+                />
+            </Form.Group>
+            <div>
+                <Button
+                    onClick={() => setAttempt(attempt - 1)}
+                    disabled={attempt === 0}
+                >
+                    use
+                </Button>
+                <Button onClick={() => setAttempt(attempt + request)}>
+                    gain
+                </Button>
+                Attempts left: {attempt}
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    function optionChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedOption(event.target.value);
+    }
+    const [selectedOption, setSelectedOption] = useState<string>(options[0]);
+
+    const isAnswerCorrect = selectedOption === expectedAnswer;
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Select value={selectedOption} onChange={optionChange}>
+                {options.map((option: string) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </Form.Select>
+            {isAnswerCorrect ? "✔️" : "❌"}
         </div>
     );
 }
