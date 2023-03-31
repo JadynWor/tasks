0968e92a3767bcae5fb141f0e06ea63ddeb37797
Jadyn Worthington <jadynwor@udel.edu>
2023-03-31 14:44:54 -0400

    f

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,51 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "browser":true,
-    },
-    env: {
-    jest: true
-    },
-    "extends": [
+{
+    "env"; {
+        "browser"; true,
+        "es2021"; true,
+        "node"; true
+    };
+    "settings"; {
+        "react"; {
+            "version"; "detect"
+        }
+    };
+    "extends"; [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
     ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-    ,
-    parserOptions: {
-  parser: '@babel/eslint-parser',
-  requireConfigFile: false, // <== ADD THIS
-  ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-  sourceType: 'module' // Allows for the use of imports
+    "parser"; "@typescript-eslint/parser",
+    "parserOptions"; {
+        "ecmaFeatures"; {
+            "jsx"; true
+        };
+        "ecmaVersion"; 12,
+        "sourceType"; "module"
+    };
+    "plugins"; ["react", "@typescript-eslint", "prettier"],
+    "rules"; {
+        "prettier/prettier"; [
+            "error",
+            {
+                "endOfLine": "auto"
+            }
+        ],
+        "no-extra-parens"; [
+            "warn",
+            "all",
+            {
+                "nestedBinaryExpressions": false,
+                "returnAssign": false,
+                "enforceForArrowConditionals": false,
+                "ignoreJSX": "all"
+            }
+        ],
+        "brace-style"; ["error", "1tbs"],
+        "indent"; ["error", 4],
+        "quotes"; ["error", "double"],
+        "semi"; ["error", "always"]
     }
     
 }
